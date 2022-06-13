6e4ccb939158991758181c5794700019715925e1
xpleiov <Sunny.helset@outlook.com>
2022-06-13 21:23:35 +0000

    add while loop for the game to run. also added interactive if else statement to change the players board.

--- run.py
+++ run.py
@@ -1,42 +1,51 @@
 from random import randint
 
+
 board = []
-
-alpha = ["A","B","C","D","E",
-         "F","G","H","I","J",
-         "K","L","M","N","O",
-         "P","Q","R","S","T",
-         "U","V","W","X","Y","Z",
-         ]
-
-numbers = [1,2,3,4,5,6,7,8,9,10,11,
-          12,13,14,15,16,17,18,19,
-          20,21,22,23,24,25,26,
-          ]
-
-alpha_length = len(alpha)
-
-grid = int(input(f"Before we start playiing,\nEnter how many grids you would like to play on\n Pick a number between 5 and {alpha_length}: "))
-
-
-for x in range(grid):
-    board.append(alpha[x] * grid)
-    for number in range(len(board)):
-            if number == 0:
-                index = 1
-                board[number] = str(index)
-                index +=1
-    
+grid = int(input("To start the game enter the grid amount you want top play with and press ENTER: "))
 
 
 def board_print(board):
+    for x in range(grid):
+        board.append(["0"] * grid)
     for row in board:
-        print(f"{0} ".join(row))
-        
-
-        
-
-
-
+        print(" ".join(row))
 
 board_print(board)
+row, column = randint(0,len(board) - 1), randint(0,len(board) - 1)
+
+# print(row, column) Used for testiing
+
+while True:
+    player_row, player_col= input("Guess Row:"), input("Guess Col:")
+
+    if player_row == "Q" or player_col == "Q":
+        print("You ended the game early")
+        break
+
+    player_row, player_col = int(player_row), int(player_col)
+
+    elif player_row == row and player_col == column:
+         board[player_row][player_col] = "X"
+         print("-"*grid * 2)
+         for row in board:
+            print(" ".join(row))
+         print("Hit!")
+         break
+
+    elif player_row > len(board) > player_col:
+        print("invalid inputs")
+
+    elif board[player_row][player_col] == "-":
+        print("You have already guessed that")
+        
+    else:
+        print("You MISSED")
+        board[player_row][player_col] = "-"
+        print("-"*grid * 2)
+        for row in board:
+            print(" ".join(row))
+
+
+
+
